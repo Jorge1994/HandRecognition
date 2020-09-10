f731c09af135a084bc4a682e4247fe40d908daae
Jorge1994 <jorgesilva.fcp.1994@gmail.com>
2020-09-10 22:36:48 +0100

    fingers

--- handRecognition.py
+++ handRecognition.py
@@ -7,6 +7,7 @@ Created on Wed Sep  9 15:58:17 2020
 
 import cv2
 import numpy as np
+import math
 
 kernel = np.ones((5, 5), np.uint8)
 
@@ -29,6 +30,36 @@ def find_contour_with_max_area(contours):
             index = i
             
     return max_area, index
+
+def find_fingers(frame, max_contour):
+    hull = cv2.convexHull(max_contour, returnPoints = False)
+    defects = cv2.convexityDefects(max_contour, hull)
+    if defects is None:
+        defects = [0]
+        num_defects = 0
+    else:
+        num_defects = 0
+        for i in range (defects.shape[0]):
+            s, e, f, d = defects[i][0]
+            start = tuple(max_contour[s][0])
+            end = tuple(max_contour[e][0])
+            far = tuple(max_contour[f][0])
+            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            angle = math.acos((b ** 2 + c ** 2 - a ** 2)/(2 * b * c))
+            semiperimenter = (a + b + c) / 2
+            herao_area = math.sqrt(semiperimenter * (semiperimenter - a) * (semiperimenter - b) * (semiperimenter - c))
+            height = (herao_area * 2) / a
+            
+            if angle <= math.pi / 2 and height > 30:
+                num_defects += 1
+                cv2.circle(frame, far, 3, (255,0,0), -1)
+                
+            cv2.line(frame, start, end, (0,0,255), 2)
+                
+        
+    return defects, num_defects
 
 def create_settings_window():
     cv2.namedWindow("Settings")
@@ -62,10 +93,11 @@ while True:
     _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0:
         _, index = find_contour_with_max_area(contours)
-        max_area = contours[index]
-        hull = cv2.convexHull(max_area)
-        cv2.drawContours(frame_copy, [max_area], 0, (0, 255, 0), 2)
+        max_contour = contours[index]
+        hull = cv2.convexHull(max_contour)
+        cv2.drawContours(frame_copy, [max_contour], 0, (0, 255, 0), 2)
         cv2.drawContours(frame_copy, [hull], 0, (0, 0, 255), 3)
+        find_fingers(frame_copy, max_contour)
     
     cv2.imshow("Output", frame_copy)
     
